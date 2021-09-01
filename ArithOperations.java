9030ce83f403e4cc91134afdf377abec1ed857e4
AdeeDeep <74358752+AdeeDeep@users.noreply.github.com>
2021-09-01 15:15:04 +0530

    Update ArithOperations.java

--- ArithOperations.java
+++ ArithOperations.java
@@ -1,15 +1,20 @@
 import Arith.*;
+import java.util.*;
 public class ArithOperations
 {
     public static void main(String args[])
     {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter 2 numbers");
+int x=sc.nextInt();
+int y=sc.nextInt();
         Add a=new Add();
         Sub s=new Sub();
         Div d=new Div();
         Mul m=new Mul();
-        System.out.println("Sum="+a.sum(2,98));
-        System.out.println("Difference="+s.difference(3,4));
-        System.out.println("Product="+m.product(3,9));
-        System.out.println("Quotient="+d.quotient(8,4));
+        System.out.println("Sum="+a.sum(x,y));
+        System.out.println("Difference="+s.difference(x,y));
+        System.out.println("Product="+m.product(x,y));
+        System.out.println("Quotient="+d.quotient(x,y));
     }
 }
